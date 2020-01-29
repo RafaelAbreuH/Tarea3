80e2de83e82bfe735b138ded427b805b89fb6e7e
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2020-01-29 11:23:59 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea3.Data;
+
+namespace Tarea3.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes>Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SQLEXPRESS; Database = Tarea3Db; Trusted_Connection = True; ");
+
+        }
+
+    }
+}
---
+++ Data/Clientes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea3.Data
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClientesId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "La Cedula es obligatoria")]
+        [StringLength(11, ErrorMessage = "La longitud de cedula debe ser de 11")]
+        public string Cedula { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefono no puede estar vacio"), Phone]
+        public string Telefono { get; set; }
+
+        public string Ciudad { get; set; }
+        public DateTime Nacimiento { get; set; }
+
+        public Clientes()
+        {
+            ClientesId = 0;
+            Nombres = String.Empty;
+            Cedula = String.Empty;
+            Email = String.Empty;
+            Telefono = String.Empty;
+            Ciudad = String.Empty;
+            Nacimiento = DateTime.Now;
+        }
+    }
+}
